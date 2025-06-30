0a5a8c4837102dd162f94a9d179c339ca45b81b7
LucioGiocondo98 <togaluc@hotmail.com>
2025-06-30 12:06:41 +0200

    Initial commit

---
+++ src/main/java/it/filminpocket/FilmInPocket/FilmInPocketApplication.java
@@ -0,0 +1,13 @@
+package it.filminpocket.FilmInPocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmInPocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmInPocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/filminpocket/FilmInPocket/FilmInPocketApplicationTests.java
@@ -0,0 +1,13 @@
+package it.filminpocket.FilmInPocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmInPocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
