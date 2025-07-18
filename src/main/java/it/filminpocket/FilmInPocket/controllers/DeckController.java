2bb0b9acc483f31ca2929c8b729644ca34677dbc
LucioGiocondo98 <togaluc@hotmail.com>
2025-07-18 15:47:40 +0200

    fix_api

--- src/main/java/it/filminpocket/FilmInPocket/controllers/DeckController.java
+++ src/main/java/it/filminpocket/FilmInPocket/controllers/DeckController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/me/decks")
+@RequestMapping("/decks")
 public class DeckController {
 
     @Autowired
